34f075a17d40fbb17d56d0fa9539c8b192552fa6
Kourieh Edouard <malkekourie@hotmail.com>
2023-06-11 22:30:14 +0200

    creation bowl

--- client/App.js
+++ client/App.js
@@ -65,6 +65,7 @@ class App extends Component{
     ],
     basket : [],
     displayDish : false,
+    displayDishFavorite : false,
     toDisplay : null,
     quantity : 1,
     displayBasket : false,
@@ -72,7 +73,8 @@ class App extends Component{
 
  constructor(props){
   super(props);
-  this.clickOn = this.clickOn.bind(this);
+  this.clickOn = this.clickOn.bind(this);//clickOnFavorite
+  this.clickOnFavorite = this.clickOnFavorite.bind(this);//clickOnFavorite
   this.closeSecondView = this.closeSecondView.bind(this);
   this.decreaseQuantity = this.decreaseQuantity.bind(this);
   this.increaseQuantity = this.increaseQuantity.bind(this);
@@ -91,12 +93,20 @@ clickOn = (plat) => {
   this.setState({ displayDish : true, toDisplay : plat})
 }
 
+clickOnFavorite = () => {
+  Animated.spring(this.pan, {
+    toValue: {x: 0, y: 0},
+    useNativeDriver: true,
+  }).start();
+  this.setState({ displayDishFavorite : true})
+}
+
 closeSecondView = () => {
   Animated.spring(this.pan, {
     toValue: {x: 0, y: 1000},
     useNativeDriver: true,
   }).start();
-  this.setState({ displayDish : false, toDisplay : null})
+  this.setState({ displayDish : false, displayDishFavorite : false,  toDisplay : null})
 }
 
 decreaseQuantity = () => {
@@ -172,7 +182,28 @@ render() {
               </Text>
             </View>
         ))    
-      }
+        }
+        <View>
+              <Text style={styles.titleCategory}  >
+              {'\n'}
+                  {"Création de Bowl"}
+                  {'\n'}
+                  <View style={styles.plat}>
+                    <TouchableOpacity   onPress={() => this.clickOnFavorite()}>
+                      <View style={styles.dish}>
+                        <View style={styles.leftDish}>
+                          <Text style={styles.titleDish}>Concoctez votre propre bowl</Text>
+                          <Text style={styles.descriptionDish}>Choisissez les ingrédients à mettre dans votre bowl</Text>
+                          <Text style={styles.priceDish}>11,50€ - 13,50€</Text>
+                        </View>
+                        <View style={styles.rightDish}>
+                          <Image source={require('./images/picture.jpg')} style={{width: 40, height: 40}} />
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+              </Text>
+            </View>
           <Modal
             animationType="slide"
             transparent={true}
@@ -195,7 +226,7 @@ render() {
                   {this.state.basket.length !== 0 ?
                   <Text>Total : {this.state.basket.reduce((a, c) => { return a + parseFloat(c.price) }, 0)} €</Text>
                   : null}
-            </View> 
+                </View> 
           </Modal>
       </ScrollView>
       {this.state.displayDish ? 
@@ -223,6 +254,32 @@ render() {
               {this.state.toDisplay === null ? "" :
                 <Button  title={"Total "+  this.price(this.state.toDisplay.price*this.state.quantity)+" €"} onPress={this.addToBasket}   />
               }
+            </Animated.View>
+          </View>
+          : null}
+        {this.state.displayDishFavorite ? 
+          <View style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
+            <Animated.View style={{
+            transform: [{translateY: this.pan.y}] ,
+            backgroundColor: 'white', 
+            position: 'absolute', 
+            top: 0, 
+            left: 0, 
+            width : '100%',
+            height : '100%',
+            zIndex: 1,
+          }}  {...this.panResponder.panHandlers}>
+            
+              <Button  title="X"  onPress={this.closeSecondView} />
+              <Image source={require('./images/picture.jpg')} style={{width: '100%', height: '20%'}} />
+              <Text  style={styles.displayDishTitle} >Création de Bowl</Text>
+              <Text  style={styles.displayDishDescription}>Choisissez vos ingrédients</Text>
+              <View style={styles.quantity}>
+                <Button  title="-"  onPress={this.decreaseQuantity}  disabled={this.state.quantity===1} />
+                <Text>{this.state.quantity}</Text>
+                <Button  title="+"  onPress={this.increaseQuantity} />
+              </View> 
+              <Button  title={"Total 10 €"} onPress={this.addToBasket}   />
             </Animated.View>
           </View>
           : null}
@@ -334,43 +391,5 @@ const styles = StyleSheet.create({
 
 /*
 
-<Modal
-            animationType="slide"
-            transparent={true}
-            visible={this.state.displayDish}
-            onRequestClose={() => {
-              Alert.alert('Modal has been closed.');
-              this.setState({displayDish: !displayDish});
-            }}
-            >
-            <View style={styles.displayDish}
-                  onStartShouldSetResponder={() => true}
-                  onResponderMove={(event) => {
-                    console.log(event.nativeEvent.locationX, event.nativeEvent.locationY);
-                  }}
-            >
-              <Button  title="X"  onPress={this.closeSecondView} />
-              <Image source={require('./images/picture.jpg')} style={{width: '100%', height: '20%'}} />
-              <Text  style={styles.displayDishTitle} >{this.state.toDisplay === null ? "" : this.state.toDisplay.name }</Text>
-              <Text  style={styles.displayDishDescription}>{this.state.toDisplay === null ? "" : this.state.toDisplay.description }</Text>
-              <View style={styles.quantity}>
-                <Button  title="-"  onPress={this.decreaseQuantity}  disabled={this.state.quantity===1} />
-                <Text>{this.state.quantity}</Text>
-                <Button  title="+"  onPress={this.increaseQuantity} />
-              </View> 
-              {this.state.toDisplay === null ? "" :
-                <Button  title={"Total "+  this.price(this.state.toDisplay.price*this.state.quantity)+" €"} onPress={this.addToBasket}   />
-              }
-            </View>
-          </Modal>
-
-*/
-
-/*
-
-onStartShouldSetResponder={() => true}
-                onResponderMove={(event) => {
-                  console.log(event.nativeEvent.locationX, event.nativeEvent.locationY);
-                }}
 
 */
