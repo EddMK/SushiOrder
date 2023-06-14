7abfaf135cc4f2f42f0368fbe600e32ad19442d0
Kourieh Edouard <malkekourie@hotmail.com>
2023-06-14 22:54:49 +0200

    own bowl

--- client/App.js
+++ client/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Animated, PanResponder, Alert,Pressable, Modal, StyleSheet,Button, Text, View, Image, ScrollView, TouchableOpacity,Dimensions, TextInput, useWindowDimensions } from 'react-native';
+import { Animated,FlatList, PanResponder, Alert,Pressable, Modal, StyleSheet,Button, Text, View, Image, ScrollView, TouchableOpacity,Dimensions, TextInput, useWindowDimensions } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { CheckBox, ListItem } from '@rneui/themed';
+//import { FlatList } from 'react-native-gesture-handler';
 
 //const Stack = createNativeStackNavigator();
 class App extends Component{
@@ -61,20 +63,132 @@ class App extends Component{
               price : 7.9
             }
           ]
-       }
+       },
+       {
+        id: 2,
+        category: 'Création de Bowl',
+        plats : [
+          {
+            id: 0,
+            name : 'Concoctez votre propre bowl',
+            description : 'Choisissez les ingrédients à mettre dans votre bowl.',
+            price : 10,
+            taille : [
+              {
+                name : "medium",
+                price : 10
+              },
+              {
+                name : "large",
+                price : 13
+              },
+            ],
+            base : [
+              {
+                name : "Mixte (riz et salade verte)",
+              },
+              {
+                name : "nachos",
+              },
+              {
+                name : "riz",
+              },
+              {
+                name : "salade de chou",
+              },
+              {
+                name : "salade verte",
+              }
+            ],
+            proteines : [
+              {
+                name : "Crispy chicken",
+              },
+              {
+                name : "saumon",
+              },
+              {
+                name : "saumon mariné",
+              },
+              {
+                name : "shrimp",
+              },
+              {
+                name : "thon",
+              },
+              {
+                name : "thon piquant",
+              },
+              {
+                name : "tofu japonais",
+              }
+            ],
+            veggie : [
+              {
+                name : "Tomates cerises",
+              },
+              {
+                name : "Mangue (+0,5€)",
+              },
+              {
+                name : "Oignons rouges",
+              },
+              {
+                name : "Carrottes",
+              },
+              {
+                name : "Salade d'algues",
+              },
+              {
+                name : "Edamame",
+              },
+              {
+                name : "Jalapeños Avocat (+0,5€)",
+              },
+              {
+                name : "Guacamole (+1€)",
+              },
+              {
+                name : "Concombre",
+              },
+              {
+                name : "Ananas",
+              },
+              {
+                name : "Cream cheese",
+              },
+              {
+                name : "Grenade",
+              },
+              {
+                name : "Coriandre",
+              },
+              {
+                name : "Menthe",
+              },
+              {
+                name : "Radis",
+              }
+            ]
+          }
+        ]
+     },
     ],
     basket : [],
     displayDish : false,
-    displayDishFavorite : false,
     toDisplay : null,
     quantity : 1,
     displayBasket : false,
+    checkTaille : null,
+    checkBase : null,
+    checkProteine : null,
+    checkVeggie : [],
+    check : true
  }
 
  constructor(props){
   super(props);
   this.clickOn = this.clickOn.bind(this);//clickOnFavorite
-  this.clickOnFavorite = this.clickOnFavorite.bind(this);//clickOnFavorite
   this.closeSecondView = this.closeSecondView.bind(this);
   this.decreaseQuantity = this.decreaseQuantity.bind(this);
   this.increaseQuantity = this.increaseQuantity.bind(this);
@@ -83,6 +197,10 @@ class App extends Component{
   this.deleteToBasket = this.deleteToBasket.bind(this);
   this.clickOnBasket = this.clickOnBasket.bind(this);
   this.hideBasket = this.hideBasket.bind(this);
+  this.checkTaille = this.checkTaille.bind(this);
+  this.checkBase = this.checkBase.bind(this);
+  this.checkVeggie = this.checkVeggie.bind(this);
+  this.checkProteine = this.checkProteine.bind(this);
  }
 
 clickOn = (plat) => {
@@ -93,20 +211,13 @@ clickOn = (plat) => {
   this.setState({ displayDish : true, toDisplay : plat})
 }
 
-clickOnFavorite = () => {
-  Animated.spring(this.pan, {
-    toValue: {x: 0, y: 0},
-    useNativeDriver: true,
-  }).start();
-  this.setState({ displayDishFavorite : true})
-}
 
 closeSecondView = () => {
   Animated.spring(this.pan, {
     toValue: {x: 0, y: 1000},
     useNativeDriver: true,
   }).start();
-  this.setState({ displayDish : false, displayDishFavorite : false,  toDisplay : null})
+  this.setState({ displayDish : false,   toDisplay : null})
 }
 
 decreaseQuantity = () => {
@@ -144,6 +255,30 @@ hideBasket = () =>{
   this.setState({ displayBasket : false})
 }
 
+checkTaille(name){
+  this.setState({checkTaille : name})
+}
+
+checkBase(name){
+  this.setState({checkBase : name})
+}
+
+checkProteine(name){
+  this.setState({checkProteine : name})
+}
+
+checkVeggie(name){
+  if(this.state.checkVeggie.includes(name)){
+    this.setState({
+      checkVeggie: this.state.checkVeggie.filter(element => {
+        return element !== name;
+      })
+    });
+  }else{
+    this.setState({ checkVeggie : [...this.state.checkVeggie, name]})
+  }
+}
+
 pan = new Animated.ValueXY();
   panResponder = PanResponder.create({
     onMoveShouldSetPanResponder: () => true,
@@ -164,7 +299,7 @@ pan = new Animated.ValueXY();
 render() {
   return (
     <View style={styles.all} >
-      <ScrollView contentContainerStyle={{flexGrow: 1}} >
+      <ScrollView contentContainerStyle={{flexGrow: 1, zIndex : 1}} >
         { this.state.menu.map((item, index) => (
             <View key = {item.id} >
               <Text key = {item.id} style={styles.titleCategory}  >
@@ -183,27 +318,6 @@ render() {
             </View>
         ))    
         }
-        <View>
-              <Text style={styles.titleCategory}  >
-              {'\n'}
-                  {"Création de Bowl"}
-                  {'\n'}
-                  <View style={styles.plat}>
-                    <TouchableOpacity   onPress={() => this.clickOnFavorite()}>
-                      <View style={styles.dish}>
-                        <View style={styles.leftDish}>
-                          <Text style={styles.titleDish}>Concoctez votre propre bowl</Text>
-                          <Text style={styles.descriptionDish}>Choisissez les ingrédients à mettre dans votre bowl</Text>
-                          <Text style={styles.priceDish}>11,50€ - 13,50€</Text>
-                        </View>
-                        <View style={styles.rightDish}>
-                          <Image source={require('./images/picture.jpg')} style={{width: 40, height: 40}} />
-                        </View>
-                      </View>
-                    </TouchableOpacity>
-                  </View>
-              </Text>
-            </View>
           <Modal
             animationType="slide"
             transparent={true}
@@ -230,22 +344,65 @@ render() {
           </Modal>
       </ScrollView>
       {this.state.displayDish ? 
-          <View style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
-            <Animated.View style={{
-            transform: [{translateY: this.pan.y}] ,
-            backgroundColor: 'white', 
-            position: 'absolute', 
-            top: 0, 
-            left: 0, 
-            width : '100%',
-            height : '100%',
-            zIndex: 1,
-          }}  {...this.panResponder.panHandlers}>
-            
+            <View style={styles.displayDish}>
+              <ScrollView contentContainerStyle={{minHeight:'200%', marginBottom:100, paddingBottom:100}}>
               <Button  title="X"  onPress={this.closeSecondView} />
               <Image source={require('./images/picture.jpg')} style={{width: '100%', height: '20%'}} />
               <Text  style={styles.displayDishTitle} >{this.state.toDisplay === null ? "" : this.state.toDisplay.name }</Text>
               <Text  style={styles.displayDishDescription}>{this.state.toDisplay === null ? "" : this.state.toDisplay.description }</Text>
+              { !this.state.toDisplay.hasOwnProperty('taille') ? null : 
+              <View>
+                <Text style={styles.titleDish}>Taille</Text>
+                {this.state.toDisplay.taille.map((item, index) => (
+                  <ListItem.CheckBox
+                  key = {index}
+                  title={item.name}
+                  checked={this.state.checkTaille === item.name ? true : false}
+                  onPress={() => this.checkTaille(item.name)}
+                  />
+                ))}
+              </View>
+              }
+              { !this.state.toDisplay.hasOwnProperty('base') ? null : 
+              <View>
+                <Text style={styles.titleDish}>Base</Text>
+                {this.state.toDisplay.base.map((item, index) => (
+                  <ListItem.CheckBox
+                  key = {index}
+                  title={item.name}
+                  checked={this.state.checkBase === item.name ? true : false}
+                  onPress={() => this.checkBase(item.name)}
+                  />
+                ))}
+              </View>
+              }
+              { !this.state.toDisplay.hasOwnProperty('proteines') ? null : 
+              <View>
+                <Text style={styles.titleDish}>Protéines (Max 1)</Text>
+                {this.state.toDisplay.proteines.map((item, index) => (
+                  <ListItem.CheckBox
+                  key = {index}
+                  title={item.name}
+                  checked={this.state.checkProteine === item.name ? true : false}
+                  onPress={() => this.checkProteine(item.name)}
+                  />
+                ))}
+              </View>
+              }
+              { !this.state.toDisplay.hasOwnProperty('veggie') ? null : 
+              <View>
+                <Text style={styles.titleDish}>Véggie (Max 3)</Text>
+                {this.state.toDisplay.veggie.map((item, index) => (
+                  <ListItem.CheckBox
+                  key = {index}
+                  disabled={this.state.checkVeggie.length === 3 && !this.state.checkVeggie.includes(item.name)  ? true: false}
+                  title={item.name}
+                  checked={this.state.checkVeggie.includes(item.name)? true : false}
+                  onPress={() => this.checkVeggie(item.name)}
+                  />
+                ))}
+              </View>
+              }
               <View style={styles.quantity}>
                 <Button  title="-"  onPress={this.decreaseQuantity}  disabled={this.state.quantity===1} />
                 <Text>{this.state.quantity}</Text>
@@ -254,34 +411,8 @@ render() {
               {this.state.toDisplay === null ? "" :
                 <Button  title={"Total "+  this.price(this.state.toDisplay.price*this.state.quantity)+" €"} onPress={this.addToBasket}   />
               }
-            </Animated.View>
-          </View>
-          : null}
-        {this.state.displayDishFavorite ? 
-          <View style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
-            <Animated.View style={{
-            transform: [{translateY: this.pan.y}] ,
-            backgroundColor: 'white', 
-            position: 'absolute', 
-            top: 0, 
-            left: 0, 
-            width : '100%',
-            height : '100%',
-            zIndex: 1,
-          }}  {...this.panResponder.panHandlers}>
-            
-              <Button  title="X"  onPress={this.closeSecondView} />
-              <Image source={require('./images/picture.jpg')} style={{width: '100%', height: '20%'}} />
-              <Text  style={styles.displayDishTitle} >Création de Bowl</Text>
-              <Text  style={styles.displayDishDescription}>Choisissez vos ingrédients</Text>
-              <View style={styles.quantity}>
-                <Button  title="-"  onPress={this.decreaseQuantity}  disabled={this.state.quantity===1} />
-                <Text>{this.state.quantity}</Text>
-                <Button  title="+"  onPress={this.increaseQuantity} />
-              </View> 
-              <Button  title={"Total 10 €"} onPress={this.addToBasket}   />
-            </Animated.View>
-          </View>
+              </ScrollView>
+            </View>
           : null}
       { this.state.basket.length !== 0 ?
         <Button title="Panier" style={styles.basket}  onPress={() => this.clickOnBasket()} />
@@ -351,8 +482,7 @@ const styles = StyleSheet.create({
   left: 0, 
   width : '100%',
   height : '100%',
-  zIndex: 1,
-  
+  zIndex: 2,
  },
  quantity:{
   flexDirection: 'row',
@@ -391,5 +521,14 @@ const styles = StyleSheet.create({
 
 /*
 
-
+<Animated.View style={{
+            transform: [{translateY: this.pan.y}] ,
+            backgroundColor: 'white', 
+            position: 'absolute', 
+            top: 0, 
+            left: 0, 
+            width : '100%',
+            height : '100%',
+            zIndex: 1,
+          }}  {...this.panResponder.panHandlers}>
 */
